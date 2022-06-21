e16da77437ac0706be1e529022d586d66a068ff0
Juan Acuña <juan0acu@gmail.com>
2022-06-20 21:45:51 -0400

    Initial Commit

---
+++ issa_detalles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  issa_detalles
+//
+//  Created by Juan Acuña on 20-06-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
